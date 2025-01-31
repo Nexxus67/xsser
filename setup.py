2ae4dbfa5d4917e32d276b67c555d6a714bbf6d8
leonplis1993@outlook.com <58827641+Nexxus67@users.noreply.github.com>
2025-01-31 15:14:40 -0300

    porting to apple sillicon

--- setup.py
+++ setup.py
@@ -21,31 +21,46 @@ Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 """
 from setuptools import setup
 import os
+
+# Inicializar listas para los archivos
 data_files = []
 image_files = []
 doc_files = []
 gtk_doc_files = []
+
+# Procesar los archivos en 'doc'
 for afile in os.listdir('doc'):
     if afile != '.svn':
         doc_files.append('doc/' + afile)
+
+# Procesar los archivos en 'gtk/docs'
 for afile in os.listdir('gtk/docs'):
     if afile != '.svn':
         gtk_doc_files.append('gtk/docs/' + afile)
-data_files = ['gtk/images/world.png', 'gtk/images/xsser.jpg',
-              'gtk/images/xssericon_16x16.png',
-              'gtk/images/xssericon_24x24.png',
-              'gtk/map/GeoIP.dat']
+
+# Archivos de imágenes y otros recursos
+data_files = [
+    'gtk/images/world.png', 'gtk/images/xsser.jpg',
+    'gtk/images/xssericon_16x16.png', 'gtk/images/xssericon_24x24.png',
+    'gtk/map/GeoIP.dat'
+]
 gtk_files = ['gtk/xsser.ui']
 gtk_app_files = ['gtk/xsser.desktop']
+
+# Configuración del paquete
 setup(
-    name = "xsser",
-    version = "1.8.4",
-    packages = ['core', 'core.fuzzing', 'core.post', 'core.driver'],
-    data_files = [('/usr/share/doc/xsser/', doc_files), 
-                  ('/usr/share/xsser/gtk/images/', data_files),
-                  ('/usr/share/xsser/gtk/docs/', gtk_doc_files),
-                  ('/usr/share/applications/', gtk_app_files),
-                  ('/usr/share/xsser/gtk/', gtk_files)],
-    scripts = ['xsser'],
-    test_suite = "tests"
+    name="xsser",
+    version="1.8.4",
+    packages=['core', 'core.fuzzing', 'core.post', 'core.driver'],
+    data_files=[
+        # Cambiar la ubicación de instalación a un directorio local en el usuario
+        (os.path.expanduser('~/.local/share/doc/xsser/'), doc_files),
+        (os.path.expanduser('~/.local/share/xsser/gtk/images/'), data_files),
+        (os.path.expanduser('~/.local/share/xsser/gtk/docs/'), gtk_doc_files),
+        (os.path.expanduser('~/.local/share/applications/'), gtk_app_files),
+        (os.path.expanduser('~/.local/share/xsser/gtk/'), gtk_files)
+    ],
+    scripts=['xsser'],
+    test_suite="tests"
 )
+
